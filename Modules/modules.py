1600b9543b6d45350175274a4eedd991a62b6ed4
GoodSoldier1P1 <danielamyx859@gmail.com>
2023-10-18 22:51:54 -0400

    week 2 day 3

---
+++ Modules/modules.py
@@ -0,0 +1,8 @@
+def square_feet(length, width):
+    """Calculate the square footage of a house"""
+    return length * width
+
+
+def circum(diameter):
+    """Calculates the circumference of a circle with the diameter"""
+    return diameter * 3.14
